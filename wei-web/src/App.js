ba3a56050e64c5f0ea306d0ac84a5533fbcd3f19
geja <gejageji@icloud.com>
2021-12-19 12:49:49 +0800

    Add flow

--- wei-web/src/App.js
+++ wei-web/src/App.js
@@ -1,7 +1,9 @@
+//@flow
+import * as React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(): React.Node {
   return (
     <div className="App">
       <header className="App-header">
