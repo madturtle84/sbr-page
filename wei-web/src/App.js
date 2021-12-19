58e1e67b5a2bf89e50815dae81af29349a06e1ab
geja <gejageji@icloud.com>
2021-12-19 13:58:06 +0800

    Add header

--- wei-web/src/App.js
+++ wei-web/src/App.js
@@ -4,23 +4,74 @@ import logo from './logo.svg';
 import './App.css';
 
 function App(): React.Node {
+  const rootStyle = {
+    height: '100vh',
+    background: 'black',
+    //display: 'flex',
+    border: 'solid cyan',
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='app' style={rootStyle}>
+      <Header />
     </div>
+  );
+}
+
+function Header(): React.Node {
+  const style = {
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'row',
+    border: 'solid red',
+    marginTop: '40px',
+    justifyContent: 'flex-end',
+  };
+  return (
+    <div style={style} >
+      <HeaderButton
+        text='Home'
+        onClick={() => {
+          console.log('Home onClick');
+        }}
+      />
+      <HeaderButton
+        text='Blog'
+        onClick={() => {
+          //TODO
+        }}
+      />
+      <HeaderButton
+        text='FAQ'
+        onClick={() => {
+          //TODO
+        }}
+      />
+      <HeaderButton
+        text='Newsletter'
+        onClick={() => {
+          //TODO
+        }}
+      />
+    </div>
+  );
+}
+
+type HeaderButtonProps = {
+  text: string,
+  onClick: () => void,
+};
+function HeaderButton(props: HeaderButtonProps): React.Node {
+  const style = {
+    backgroundColor: 'inherit',
+    fontSize: 20,
+    color: 'rgb(155,155,155)',
+    border: 'solid yellow',
+    marginLeft: '20px',
+    marginRight: '20px',
+    cursor: 'pointer',
+  };
+  return (
+    <button style={style} onClick={props.onClick}>{props.text}  </button>
   );
 }
 
