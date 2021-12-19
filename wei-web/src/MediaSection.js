8bcba0a636c47a5ea77bacd70025639d752a0802
geja <gejageji@icloud.com>
2021-12-19 16:56:25 +0800

    Add carousel

--- wei-web/src/MediaSection.js
+++ wei-web/src/MediaSection.js
@@ -1,6 +1,12 @@
 //@flow
 import * as React from 'react';
 import Title from './Title';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import carrousel_1 from './images/carousel/carousel_1.png';
+import carrousel_2 from './images/carousel/carousel_2.png';
+import carrousel_3 from './images/carousel/carousel_3.png';
+import carrousel_4 from './images/carousel/carousel_4.png';
 
 export default function MediaSection(): React.Node {
   const containerStyle = {
@@ -8,7 +14,25 @@ export default function MediaSection(): React.Node {
   };
   return (
     <div style={containerStyle}>
-      <Title text='Media' marginTop={60} />
+      <Title text='Media'/>
+      <Carousel>
+        <div>
+            <img src={carrousel_1} alt='carousel_1' />
+            <p className="legend">Legend 1</p>
+        </div>
+        <div>
+            <img src={carrousel_2} alt='carousel_2' />
+            <p className="legend">Legend 2</p>
+        </div>
+        <div>
+            <img src={carrousel_3} alt='carousel_3' />
+            <p className="legend">Legend 3</p>
+        </div>
+        <div>
+            <img src={carrousel_4} alt='carousel_4' />
+            <p className="legend">Legend 4</p>
+        </div>
+      </Carousel>
     </div>
   );
 }
