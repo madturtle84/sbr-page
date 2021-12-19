b1ea1a628666c13b1060bfe3d46d3153a91cabba
geja <gejageji@icloud.com>
2021-12-19 14:42:23 +0800

    Add poster section

--- wei-web/src/App.js
+++ wei-web/src/App.js
@@ -1,6 +1,6 @@
 //@flow
 import * as React from 'react';
-import logo from './logo.svg';
+import posterImage from './images/poster.png';
 import './App.css';
 
 function App(): React.Node {
@@ -13,6 +13,7 @@ function App(): React.Node {
   return (
     <div id='app' style={rootStyle}>
       <Header />
+      <PosterSection />
     </div>
   );
 }
@@ -72,6 +73,48 @@ function HeaderButton(props: HeaderButtonProps): React.Node {
   };
   return (
     <button style={style} onClick={props.onClick}>{props.text}  </button>
+  );
+}
+
+function PosterSection(): React.Node {
+  const divStyle = {
+    display: 'flex',
+    position: 'relative',
+    justifyContent: 'center',
+    border: 'solid red',
+  };
+  const imgStyle = {
+    border: 'solid green',
+    maxWidth: '100%',
+  };
+  return (
+    <div style={divStyle}>
+      <img style={imgStyle} src={posterImage} alt='homeMain' />      
+      <PosterSectionButton />
+    </div>
+  )
+}
+
+function PosterSectionButton(): React.Node {
+  const style = {
+    position: 'absolute',
+    bottom: '40px',
+    backgroundColor: '#455548',
+    fontSize: 20,
+    textAlign: 'center',
+    color: 'white',
+    cursor: 'pointer',
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingLeft: 20,
+    paddingRight: 20,
+    border: 'solid red',
+  };
+  function onClick() {
+
+  }
+  return (
+    <button style={style} onClick={onClick}>Get Newsletter</button>
   );
 }
 
